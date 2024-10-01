161ca755096c3ffca1276c7a9c5a199ea6d51aaa
Christian Blais <christ.blais@gmail.com>
2024-09-30 21:11:04 -0400

    Sentences

--- rolling-word.mjs
+++ rolling-word.mjs
@@ -1,91 +1,129 @@
-import './utils.mjs';
-
 export class RollingWord extends HTMLElement {
   ALPHABET = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', ' '];
 
-  constructor() {
-    super();
+  static observedAttributes = ['list', 'speed', 'duration', 'alphabet', 'length'];
 
-    /**
-     * The list of options picked from the specified datalist
-     * @type NodeListOf<HTMLOptionElement>
-     */
-    let options;
+  /**
+   * The list of available words
+   * @type string[]
+   */
+  #words;
 
-    if (this.hasAttribute('list')) {
-      options = document.querySelectorAll(`#${this.getAttribute('list')} option`);
-    } else {
-      options = this.querySelectorAll('datalist option');
+  /**
+   * The speed at which we swap characters, in milliseconds
+   * @type Number
+   */
+  #speed;
+
+  /**
+   * Duration of the rolling effect before settling on a word, in milliseconds
+   * @type Number
+   */
+  #duration;
+
+  /**
+   * Available alphabet from which we can pick random chars
+   * @type string[]
+   */
+  #alphabet;
+
+  /**
+   * The number of rolling chars to generate
+   * @type Number
+   */
+  #length;
+
+  /**
+   * Flag to determine whether or not this component is active
+   * @type boolean
+   */
+  #isActive = false;
+
+  /**
+   * References to the setInterval used to roll characters
+   * @type number
+   */
+  #interval;
+
+  attributeChangedCallback(name, _, value) {
+    switch (name) {
+      case 'list':
+        this.#words = Array.from(document.querySelectorAll(`#${this.getAttribute('list')} option`)).map(option => option.innerText);
+        break;
+      case 'speed':
+        this.#speed = Number(value);
+        break;
+      case 'duration':
+        this.#duration = Number(value);
+        break;
+      case 'duration':
+        this.#duration = Number(value);
+        break;
+      case 'alphabet':
+        this.#alphabet = value.split(',');
+        break;
+      case 'length':
+        this.#length = Number(value);
+        break;
     }
-
-    if (options.length === 0) {
-      throw new SyntaxError('RollingWord elements must define a datalist');
-    }
-
-    /**
-     * The list of available words
-     * @type string[]
-     */
-    this.words = Array.from(options).map(option => option.innerText);
-
-    /**
-     * The speed at which we swap characters, in milliseconds
-     * @type Number
-     */
-    this.speed = this.hasAttribute('speed') ? Number(this.getAttribute('speed')) : 100;
-
-    /**
-     * Duration of the rolling effect before settling on a word, in milliseconds
-     * @type Number
-     */
-    this.duration = this.hasAttribute('duration') ? Number(this.getAttribute('duration')) : 750;
-
-    /**
-     * Available alphabet from which we can pick random chars
-     * @type Number
-     */
-    this.alphabet = this.hasAttribute('alphabet') ? this.getAttribute('alphabet').split(',') : this.ALPHABET;
-
-    /**
-     * The number of rolling chars to generate
-     * @type Number
-     */
-    this.chars = this.hasAttribute('chars') ? Number(this.getAttribute('chars')) : 10;
-
-    /**
-     * Flag to determine whether or not this component is active
-     * @type boolean
-     */
-    this.isActive = false;
   }
 
   connectedCallback() {
+    // While the list of available words might be provided by a `list` attribute,
+    // it may also be defined with an inner datalist tag. The list has priority.
+    this.#words ??= Array.from(this.querySelectorAll(`datalist option`)).map(option => option.innerText);
+
     // Upon instantiation, start rolling a first word
     this.roll();
   }
 
+  get words() {
+    return this.#words;
+  }
+
+  get speed() {
+    return this.#speed ?? 100;
+  }
+
+  get duration() {
+    return this.#duration ?? 1000;
+  }
+
+  get length() {
+    return this.#length ?? 10;
+  }
+
+  get alphabet() {
+    return this.#alphabet ?? this.ALPHABET;
+  }
+
+  /**
+   * @param array {Array} An array
+   * @returns {any} An element picked at random from array
+   */
+  #pick(array) {
+    return array[Math.floor(Math.random() * array.length)];
+  }
+
   /**
    * Update the word with some random characters
-   * @private
    */
-  tick() {
-    // Update the text with some random characters
-    this.innerText = [...Array(this.chars).keys()].map(() => this.alphabet.pick()).join('');
+  #tick() {
+    this.innerText = [...Array(this.length).keys()].map(() => this.#pick(this.alphabet)).join('');
   }
 
   /**
    * Stop rolling characters and stabilize on a given word
-   * @private
    */
-  done() {
+  #done() {
     // Make sure we stop rolling the characters
-    clearInterval(this.interval);
+    clearInterval(this.#interval);
 
     // Stabilize with one of the available words, picked at random
-    this.innerText = this.words.pick();
+    this.innerText = this.#pick(this.words);
 
     // Indicate that we're done
-    this.isActive = false;
+    this.#isActive = false;
   }
 
   /**
@@ -93,15 +131,17 @@ export class RollingWord extends HTMLElement {
    */
   roll() {
     // If we're currently active, abort, and let the previous roll complete
-    if (this.isActive) { return }
+    if (this.#isActive) { return }
 
     // Setup the routine to start rolling the characters
-    this.interval = setInterval(this.tick.bind(this), this.speed);
+    this.#interval = setInterval(this.#tick.bind(this), this.speed);
 
     // Setup the routine to stabilize the word once we're done
-    setTimeout(this.done.bind(this), this.duration);
+    setTimeout(this.#done.bind(this), this.duration);
 
     // Indicate that we're currently rolling
-    this.isActive = true;
+    this.#isActive = true;
   }
 }
+
+window.customElements.define('rolling-word', RollingWord);
--- utils.mjs
+++
@@ -1,6 +0,0 @@
- /**
-  * @returns An element picked at random
-  */
-Array.prototype.pick = function() {
-  return this[Math.floor(Math.random() * this.length)];
-}
